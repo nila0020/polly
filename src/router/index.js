f31c2b12f8c32adca5fb9ca1c669f3511c75179b
pictorviktor <viktor.e.evestam@gmail.com>
2021-11-26 16:32:58 +0100

    Placerade om rutorna på user

--- src/router/index.js
+++ src/router/index.js
@@ -29,7 +29,7 @@ const routes = [
     component: () => import(/* webpackChunkName: "about" */ '../views/Result.vue')
   },
   {
-    path: '/user',
+    path: '/user/',
     name: 'User',
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
--- src/views/User.vue
+++ src/views/User.vue
@@ -1,15 +1,28 @@
 <template>
-<label for="">GameID</label><br>
-<input type="text" id="gameId" v-model="gameId" required="required" placeholder="Write your GameId">
-
-
-<label for="">Your game username</label><br>
-<input type="text" id="userName" v-model="userName" required="required" placeholder="Your username">
-
+<div class="entryID">
+<div>    
+<label for="GameId">GameID</label><br>
+<input type="text" id="gameId" v-model="gameId" required="required" placeholder="Write your GameId here">
+</div>
+<div>
+<label for="">Username</label><br>
+<input type="text" id="userName" v-model="userName" required="required" placeholder="Write your username here">
+</div>
+</div>
 </template>
 
 <script>
 </script>
 
 <style>
+.entryID {
+  
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  justify-content: center;
+  width: 100%;
+  justify-content: space-evenly;
+  /* align-content: space-between; */
+}
 </style>
