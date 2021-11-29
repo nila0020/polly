1bb0662054f43969b60491d7d6385602c99ee4d9
felixt <Felix47@hotmail.se>
2021-11-29 15:56:06 +0100

    första push poll

--- src/views/Poll.vue
+++ src/views/Poll.vue
@@ -1,11 +1,13 @@
 <template>
-hejhej
   <div>
-    {{pollId}}
     <Question v-bind:question="question"
               v-on:answer="submitAnswer"/>
   </div>
-  
+  <div class="wrapper">
+   <div class="box a"> {{pollId}}</div>
+   <div class="box b" id="map"></div>
+   
+ </div>
 </template>
 
 <script>
@@ -13,6 +15,7 @@ hejhej
 import Question from '@/components/Question.vue';
 import io from 'socket.io-client';
 const socket = io();
+
 
 
 export default {
@@ -43,3 +46,40 @@ export default {
   }
 }
 </script>
+<style>
+
+.wrapper {
+    
+     display: grid;
+     width: 100vw;
+     height:100vh;
+      grid-template-rows: 8% 88%; 
+      grid-gap: 2%;
+      background-color: #fff;
+      color: #444;
+ }
+
+ .box {
+     background-color: #444;
+     color: #fff;
+     border-radius: 5px;
+     padding: 0px;
+     font-size: 150%;
+ }
+
+ .a {
+     grid-column: 1 ;
+     grid-row:1;
+ }
+ .b {
+      grid-column: 1 ;
+      grid-row: 2;
+      overflow:scroll;
+      position: relative;
+ }    
+    
+#map {
+        background: url("/img/polacks.jpg");
+  }
+
+ </style>
