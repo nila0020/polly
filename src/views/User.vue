ca1889ab0e3424a33aa5e31b9bfa78f7824f80ba
felixt <Felix47@hotmail.se>
2021-11-29 16:55:58 +0100

    committar user

--- src/views/User.vue
+++ src/views/User.vue
@@ -1,18 +1,27 @@
 <template>
 <div class="entryID">
 <div>    
-<label for="GameId">GameID</label><br>
-<input type="text" id="gameId" v-model="gameId" required="required" placeholder="Write your GameId here">
+<label for="pollId">Poll-Code</label><br>
+<input type="text" id="pollId" v-model="pollId" required="required" placeholder="Write your pollId here">
 </div>
 <br><br><br><br>
 <div>
 <label for="">Username</label><br>
 <input type="text" id="userName" v-model="userName" required="required" placeholder="Write your username here">
 </div>
+<router-link v-bind:to="'/poll/'+pollId" tag="button"><v-btn class="start_buttons">Join GeoQuiz!</v-btn></router-link>
 </div>
 </template>
 
 <script>
+export default {
+  name: 'User',
+  data: function () {
+    return {
+      pollId:""
+     }
+  }
+}
 </script>
 
 <style>
