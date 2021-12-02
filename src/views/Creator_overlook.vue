0b6bad3883e532291d0d940b058d4cb98df235a3
pictorviktor <viktor.e.evestam@gmail.com>
2021-12-02 14:46:44 +0100

    fixade titlebox

--- src/views/Creator_overlook.vue
+++ src/views/Creator_overlook.vue
@@ -3,11 +3,15 @@
       <!--Title box-->
       <div class="box titleBox" >
         <h3>Game Title and GameID</h3>
-        <input type="text" v-model="pollName" placeholder="Game name">
-        <input type="text" v-model="pollId" placeholder="Game ID">
-        <button v-on:click="createPoll">
-          Create poll
+        <div class="insertTitle">
+        <label for = "gameName">Game name: </label>
+        <input type="text" id = "gameName" v-model="pollName" placeholder="Enter Game name"><br>
+        <label for = "gameID">GameID: </label>
+        <input type="text" id = "gameID" v-model="pollId" placeholder="Enter Game ID"><br><br>
+        <button class = "createButton" v-on:click="createPoll">
+          Create Game
         </button>
+        </div>
       </div>
 
       <!--Overlook box-->
@@ -152,9 +156,16 @@ export default {
   padding: 20px;
   font-size: 150%;
 }
+.insertTitle {
+  float: right;
+}
+.titleBox h3 {
+  float: left;
+}
 .titleBox {
   grid-column: 1/ span 4;
   grid-row: 1;
+  
 }
 .Overlook {
   grid-column: 1;
@@ -183,5 +194,54 @@ export default {
 .map {
   grid-column: 1 /span 2;
   grid-row: 2;
+}
+.createButton {
+  float:right;
+  align-items: center;
+  background-image: linear-gradient(144deg,#AF40FF, #5B42F3 50%,#00DDEB);
+  border: 0;
+  border-radius: 8px;
+  box-shadow: rgba(151, 65, 252, 0.2) 0 15px 30px -5px;
+  box-sizing: border-box;
+  color: #FFFFFF;
+  display: flex;
+  font-family: Phantomsans, sans-serif;
+  font-size: 20px;
+  justify-content: center;
+  line-height: 2em;
+  max-width: 100%;
+  min-width: 140px;
+  padding: 3px;
+  text-decoration: none;
+  user-select: none;
+  -webkit-user-select: none;
+  touch-action: manipulation;
+  white-space: nowrap;
+  cursor: pointer;
+}
+
+.createButton:active,
+.createButton:hover {
+  outline: 0;
+}
+
+.createButton span {
+  background-color: rgb(5, 6, 45);
+  padding: 16px 24px;
+  border-radius: 6px;
+  width: 100%;
+  height: 45%;
+  transition: 300ms;
+}
+
+.createButton:hover span {
+  background: none;
+}
+
+@media (min-width: 768px) {
+  .createButton {
+    font-size: 10px;
+    min-width: 90px;
+  }
 }
 </style>
