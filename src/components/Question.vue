04d3c9e2c4ef6818c4aab4348701b8951afb2acb
felixt <Felix47@hotmail.se>
2021-12-04 21:08:20 +0100

    fixat en konditionaliserad frågevy

--- src/components/Question.vue
+++ src/components/Question.vue
@@ -1,19 +1,75 @@
 <template>
-<p>{{question.q}}</p>
-<button v-for="a in question.a" v-on:click="answer(a)" v-bind:key="a">
-  {{ a }}
-</button>
+  <div class="outerGrid">
+    <div class="picture">
+      <img
+        v-bind:src="'https://upload.wikimedia.org/wikipedia/commons/thumb/9/9e/Uppsala_Cathedral_in_February.jpg/1920px-Uppsala_Cathedral_in_February.jpg'"
+        style="width: 100%; height: 100%; object-fit: cover"
+      />
+    </div>
+    <div class="questionBar">
+      {{ question.q }}
+    </div>
+    <div class="answerGrid">
+      <v-btn
+        v-for="a in question.a"
+        v-on:click="answer(a)"
+        v-bind:key="a"
+        class="answerButtons"
+      >
+        {{ a }}
+      </v-btn>
+    </div>
+  </div>
 </template>
 <script>
 export default {
-  name: 'Bars',
+  name: "Bars",
   props: {
-    question: Object
+    question: Object,
   },
   methods: {
     answer: function (answer) {
       this.$emit("answer", answer);
-    } 
-  }
-}
+    },
+  },
+};
 </script>
+<style scoped>
+.outerGrid {
+  display: grid;
+  width: 100vw;
+  height: 100vh;
+  grid-template-rows: 40% 10% 45%;
+  grid-gap: 2vh;
+  background-color: white;
+  color: black;
+}
+.questionBar {
+  color: white;
+  background-color: purple;
+  border: 0px;
+  border-radius: 1em;
+}
+.answerGrid {
+  display: grid;
+  width: 100%;
+  height: 100%;
+  grid-template-columns: 1fr 1fr;
+  grid-gap: 2vh;
+  background-color: white;
+  color: black;
+  overflow: hide;
+}
+.answerButtons {
+  background-color: red;
+  border: 0px;
+  border-radius: 1em;
+}
+.picture {
+  width: 100%;
+  object-fit: fill;
+  overflow: hidden;
+  border: 0px;
+  border-radius: 1em;
+}
+</style>
--- src/views/Poll.vue
+++ src/views/Poll.vue
@@ -1,40 +1,56 @@
 <template>
-<div v-show="!confirmedUser" class="entryID">
-  <h1>Lets GO!</h1>
-  <div class="boxA">    
-  <label for="pollId" class="start_buttons1">Poll-ID</label><br>
-  <input type="text" style="font-size:1.4em" id="pollId" v-model="pollId" required="required" placeholder="Input the poll-Id">
+  <div v-show="!confirmedUser" class="entryID">
+    <h1>Lets GO!</h1>
+    <div class="boxA">
+      <label for="pollId" class="start_buttons1">Poll-ID</label><br />
+      <input
+        type="text"
+        style="font-size: 1.4em"
+        id="pollId"
+        v-model="pollId"
+        required="required"
+        placeholder="Input the poll-Id"
+      />
+    </div>
+    <br /><br /><br /><br />
+    <div class="boxB">
+      <label for="" class="start_buttons1">Username</label><br />
+      <input
+        type="text"
+        style="font-size: 1.4em"
+        id="userName"
+        v-model="userName"
+        required="required"
+        placeholder="Input your username"
+      />
+    </div>
+    <div class="boxC">
+      <v-btn class="start_buttons1" v-on:click="confirmUser"
+        >Join GeoQuiz!</v-btn
+      >
+    </div>
   </div>
-  <br><br><br><br>
-  <div class="boxB">
-  <label for="" class="start_buttons1">Username</label><br>
-  <input type="text" style="font-size:1.4em" id="userName" v-model="userName" required="required" placeholder="Input your username">
-  </div>
-  <div class="boxC">
-  <v-btn class="start_buttons1" v-on:click="confirmUser">Join GeoQuiz!</v-btn>
-</div>
-</div>
   <div v-show="confirmedUser" class="fullFrame">
-     {{question}}
-  <section class="overview">
-  <div class="questions">
-    <Question v-bind:question="question"
-              v-on:answer="submitAnswer"/>
+    <div v-show="!activeQuestion" class="overview">
+      <v-btn class="showquestion box b" v-on:click="activateQuestion"
+        >question!</v-btn
+      >
+      <div class="wrapper">
+        <div class="box a">{{ pollId }}</div>
+
+        <div class="box b">
+          <div class="wrapmap">
+            <div id="map"></div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div v-show="activeQuestion" class="questionDisplayed">
+      <Question v-bind:question="question" v-on:answer="submitAnswer" />
+    </div>
   </div>
-  <div class="wrapper">
-   <div class="box a start_buttons1"> {{pollId}}</div>
-   
-   <div class="box b" >
-   <div class="wrapmap">
-      <div id="map">
-      </div>
-      </div>
-   </div>
-   
- </div>
- </section>
-  <section class="activeQuestion">
-    <!--först ta reda på vilken frågetyp det är
+
+  <!--först ta reda på vilken frågetyp det är
         ha metoder för att rita upp varje frågetyp
         ex
         on:click -> display:true ->,
@@ -53,106 +69,98 @@ Question:{
   this.pic=url
   this.correctAnswer=1300               (alternativ eller värde beroende på frågetyp)
   }}
--->
-  </section>
- </div>
-</template>
+--></template>
 
 <script>
 // @ is an alias to /src
-import Question from '@/components/Question.vue';
-import io from 'socket.io-client';
+import Question from "@/components/Question.vue";
+import io from "socket.io-client";
 const socket = io();
 
-
 export default {
-  name: 'Poll',
+  name: "Poll",
   components: {
-    Question
+    Question,
   },
   data: function () {
     return {
       question: {
         q: "",
         a: [],
-        info: ""
+        info: "",
       },
       pollId: "inactive poll",
       confirmedUser: false,
-    }
+      activeQuestion: false,
+    };
   },
   created: function () {
-    this.pollId = this.$route.params.id
-    socket.on("newQuestion", q =>
-      this.question = q
-    )
+    this.pollId = this.$route.params.id;
+    socket.on("newQuestion", (q) => (this.question = q));
   },
   methods: {
     submitAnswer: function (answer) {
-      socket.emit("submitAnswer", {pollId: this.pollId, answer: answer})
+      socket.emit("submitAnswer", { pollId: this.pollId, answer: answer });
     },
     confirmUser: function () {
       this.confirmedUser = true;
-      socket.emit('joinPoll', this.pollId)
-    }
-  }
-}
+      socket.emit("joinPoll", this.pollId);
+    },
+    activateQuestion: function () {
+      this.activeQuestion = true;
+    },
+  },
+};
 </script>
 <style>
-.fullFrame{
-  display:flex;
-  flex-direction:row;
-  overflow:hidden;
-  border:0px;
-  border-radius:8px;}
-
+.fullFrame {
+}
 .wrapper {
-    
-     display: grid;
-     width: 100vw;
-     height:100vh;
-      grid-template-rows: 8% 88%;
-      grid-gap: 2%;
-      background-color: white;
-      color: black;
- }
+  display: grid;
+  width: 100%;
+  height: 100%;
+  grid-template-rows: 1fr 9fr;
+  grid-gap: 2%;
+  background-color: white;
+  color: black;
+}
 
- .box {
-     background-color: white;
-     color: black;
-     border-radius: 5px;
-     padding: 0px;
-     font-size: 150%;
- }
+.box {
+  background-color: white;
+  color: black;
+  border-radius: 12px;
+  padding: 0px;
+  font-size: 150%;
+}
 
- .a {
-     grid-column: 1 ;
-     grid-row:1;
-     color:white;
-     width:96%;
-     background-color:purple;
- }
- .b {
-      grid-column: 1 ;
-      grid-row: 2;
-      width:100vw;
- }    
-.wrapmap{
-  width:96%;
-  height:100%;
-  overflow:scroll;
+.a {
+  grid-column: 1;
+  grid-row: 1;
+  color: white;
+  width: 96%;
+  background-color: purple;
+}
+.b {
+  grid-column: 1;
+  grid-row: 2;
+  width: 100vw;
+}
+.wrapmap {
+  width: 96%;
+  height: 100%;
+  overflow: scroll;
   position: relative;
-  border:0px;
-  border-radius:12px;
+  border: 0px;
+  border-radius: 12px;
 }
 #map {
-        width:1920px;
-        height:1080px;
-        background: url("/img/polacks.jpg");
-        border:0px;
-        border-radius:8px;
-  }
- .questions {
-   position:absolute;
- }
- </style>
+  width: 1920px;
+  height: 1080px;
+  background: url("/img/polacks.jpg");
+  border: 0px;
+  border-radius: 8px;
+}
+.questionDisplayed {
+  width: 100%;
+}
+</style> 
