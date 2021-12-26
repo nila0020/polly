87f1e99af69c9e65400dd12687341688c6191b69
pictorviktor <viktor.e.evestam@gmail.com>
2021-12-26 16:10:16 +0100

    donat med markers

--- src/views/Creator_overlook.vue
+++ src/views/Creator_overlook.vue
@@ -257,34 +257,77 @@ export default {
   components: {
     Slider,
   },
-  setup() {
-    let myMap;
-    onMounted(() => {
-      myMap = leaflet.map("myMap").setView([59.855727, 17.633445], 13);
+     setup() {
+     
+ 
+    //Load map
 
-      leaflet
-        .tileLayer(
-          "https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoicGljdG9ydmlrdG9yIiwiYSI6ImNreGM4aW43ZjRkNzUydXFvYnB5eDZ3d3MifQ.gSVvXd28nfGeuWEnHdIEhQ",
-          {
-            attribution:
-              'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-            maxZoom: 18,
-            id: "mapbox/streets-v11",
-            tileSize: 512,
-            zoomOffset: -1,
-            accessToken:
-              "pk.eyJ1IjoicGljdG9ydmlrdG9yIiwiYSI6ImNreGM4aW43ZjRkNzUydXFvYnB5eDZ3d3MifQ.gSVvXd28nfGeuWEnHdIEhQ",
-          }
-        )
-        .addTo(myMap);
+      let myMap;
+      onMounted(()=>{
+        myMap = leaflet.map('myMap').setView([59.855727, 17.633445], 13);
+       
+        leaflet.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoicGljdG9ydmlrdG9yIiwiYSI6ImNreGM4aW43ZjRkNzUydXFvYnB5eDZ3d3MifQ.gSVvXd28nfGeuWEnHdIEhQ', {
+        attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+        maxZoom: 18,
+        id: 'mapbox/streets-v11',
+        tileSize: 512,
+        zoomOffset: -1,
+        accessToken: 'pk.eyJ1IjoicGljdG9ydmlrdG9yIiwiYSI6ImNreGM4aW43ZjRkNzUydXFvYnB5eDZ3d3MifQ.gSVvXd28nfGeuWEnHdIEhQ',
+        
+}).addTo(myMap);
+// Distance function
+function checkDistance(a,b) {
+          if ( myMap.distance(a,b) < 30) {
+            alert('Within range')}
+            console.log('avstånd i meter',myMap.distance(a,b))
+    
+}
 
-      myMap.on("click", function (e) {
-        new leaflet.Marker([e.latlng.lat, e.latlng.lng]).addTo(myMap);
-      });
-    });
-  },
+// Find user location
+setInterval(() => {
+  
+  navigator.geolocation.getCurrentPosition(getPosition)
+  
+},5000
+); 
+var lat,lng, marker, latLng
+
+function getPosition(position) {
+   
+  if (marker) {
+    myMap.removeLayer(marker)
+  }
+  
+  lat = position.coords.latitude
+  lng = position.coords.longitude
+  latLng = [lat, lng]
+marker = leaflet.marker([lat, lng]).addTo(myMap)
+
+return latLng;
+}
+
+// Current distance from user location
+setTimeout(() => console.log(checkDistance(latLng,[59.855727, 17.633445])), 6000);
+setTimeout(() => console.log('Marker din position utanför',latLng), 6000);
+
+// Create marker on map
+myMap.on("click", function(e){
+      var marker = new leaflet.marker([e.latlng.lat, e.latlng.lng]).addTo(myMap);
+      this.pos = [e.latlng.lat, e.latlng.lng];
+      console.log('onClick marker',marker)
+       console.log('Position',this.pos)
+      return this.pos
+ 
+          
+    
+        }
+       
+      );
+   }) 
+},
   data: function () {
     return {
+      pos: [],
       questionText: "", // detta är textrutan i overlook - Den funktionen ska vara i questionbox
       questions: [],
       info: "",
